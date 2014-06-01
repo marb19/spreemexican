5d450de940b71a79e81a2a9fbbad376167d747f5
vagrant <vagrant@precise64.(none)>
2014-06-01 22:43:02 +0000

    Creating rails application

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Spreemexican::Application.config.secret_key_base = '54f2540c3009dc497d2a05b51f0a8bf21d61d6e597614ac6ad5b9bddf37c7147b51b53db05ef2e55f27b57020a6250e12634d6d50b391f401cb88b5ea7ecda4c'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Spreemexican::Application.config.session_store :cookie_store, key: '_spreemexican_session'
